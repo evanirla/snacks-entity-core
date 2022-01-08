55d9a4d6266a1a21495a3152d7325c9f8f45be45
Evan Irla <ageekstouch@gmail.com>
2022-01-08 14:34:57 -0700

    Simplified dependency injection

--- test/Snacks.Entity.Core.Tests/Server/Services/ItemService.cs
+++ test/Snacks.Entity.Core.Tests/Server/Services/ItemService.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Snacks.Entity.Core.Tests.Server.Database;
+﻿using Snacks.Entity.Core.Tests.Server.Database;
 using Snacks.Entity.Core.Tests.Server.Models;
+using System;
 
 namespace Snacks.Entity.Core.Tests.Server.Services
 {
     public class ItemService : EntityServiceBase<ItemModel, GlobalDbContext>
     {
         public ItemService(
-            IServiceScopeFactory scopeFactory) : base(scopeFactory)
+            IServiceProvider serviceProvider) : base(serviceProvider)
         {
 
         }
